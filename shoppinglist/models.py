26d42100a813a6b71ec73ed68713fca4b0762b84
Yuuki Shimizu <yuuki@maxio.jp>
2019-11-15 00:11:10 +0900

    [add] Item モデルの作成

--- shoppinglist/models.py
+++ shoppinglist/models.py
@@ -8,3 +8,15 @@ class Shop(models.Model):
 
     def __str__(self):
         return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    item_url = models.URLField(blank=True,null=True)
+    count = models.PositiveIntegerField(default=0)
+    buy_date = models.DateField(blank=True,null=True)
+    shop = models.ForeignKey(Shop,blank=True,null=True,
+                             verbose_name='shop', on_delete=models.PROTECT)
+    buy = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{} ({})'.format(self.name, self.buy_date)
