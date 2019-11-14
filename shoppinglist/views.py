11c8d3a43a72f7bb842c48befaab93af53f36af5
Yuuki Shimizu <yuuki@maxio.jp>
2019-11-15 01:02:46 +0900

    [add] ItemListクラスの post リクエストに対する動作を加える

--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -1,5 +1,22 @@
 from django.views.generic import ListView
 from .models import Item
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import get_object_or_404
+from .forms import ItemBuy
 
 class ItemList(ListView):
     model = Item
+
+    def post(self, request, *args, **kwargs):
+        item_id = self.request.POST.get('item_id')
+        item = get_object_or_404(Item, pk=item_id)
+        item_status = self.request.POST.get('item_status')
+        item.buy = item_status
+        item.save()
+        return HttpResponseRedirect(reverse('list'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ItemBuy()
+        return context
