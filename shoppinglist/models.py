cffb46c8688b331fc39ef26a47342422a078f3e3
Yuuki Shimizu <yuuki@maxio.jp>
2019-11-15 00:09:35 +0900

    [add] Shop モデルの作成

--- shoppinglist/models.py
+++ shoppinglist/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    shop_site = models.URLField(blank=True,null=True)
+
+    def __str__(self):
+        return self.name
