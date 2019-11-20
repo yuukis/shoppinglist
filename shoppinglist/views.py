1499540a359b5c6d6c2445bfae52f18c2a7a3126
Yuuki Shimizu <yuuki@maxio.jp>
2019-11-20 14:37:06 +0900

    [add] Showビューを実装

--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -23,11 +23,52 @@ class ItemList(ListView):
         context['form'] = ItemBuy()
         return context
 
+class ItemShowView(TemplateView):
+    model = Item
+    template_name = 'shoppinglist/item_show.html'
+
+    def post(self, request, *args, **kwargs):
+        item_id = self.request.POST.get('item_id')
+        item = Item.objects.get(pk=item_id)
+        context = super().get_context_data(**kwargs)
+        context['form_id'] = ItemIdForm()
+        context['item'] = item
+        return self.render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form_id'] = ItemIdForm()
+        return context
+
 class ItemAddView(CreateView):
     model = Item
     fields = ('name', 'item_url', 'count', 'buy_date', 'shop')
     template_name = 'shoppinglist/item_add.html'
     success_url = 'list/'
+
+class ItemEditView(TemplateView):
+    model = Item
+    template_name = 'shoppinglist/item_edit.html'
+    success_url = 'list/'
+
+    def post(self, request, *args, **kwargs):
+        item_id = self.request.POST.get('item_id')
+        name = self.request.POST.get('name')
+        count = self.request.POST.get('count')
+        buy_date = self.request.POST.get('buy_date')
+
+        item = get_object_or_404(Item, pk=item_id)
+        item.name = name
+        item.count = count
+        item.buy_date = buy_date
+        item.save()
+        return HttpResponseRedirect(reverse('list'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form_id'] = ItemIdForm()
+        context['form'] = ItemForm()
+        return context
 
 class ItemDeleteView(TemplateView):
     model = Item
