1857beee4b2349142199eecd3b330e778c2e3baf
Yuuki Shimizu <yuuki@maxio.jp>
2019-11-20 13:13:39 +0900

    [modify] ItemDeleteView が呼ばれるよう、urls.py を変更

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ItemList
+from .views import ItemList, ItemDeleteView
 
 appname='shoppinglist'
 urlpatterns = [
     path('list/', ItemList.as_view(),name='list'),
-    path('delete', ItemList.as_view(),name='delete'),
+    path('delete', ItemDeleteView.as_view(),name='delete'),
 ]
