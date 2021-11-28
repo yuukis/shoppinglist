3d19d271e36e7e8beb4b221c7a4b9bd7104292de
YUAN YINGXUAN <t19cs012@yamanashi.ac.jp>
2021-11-29 04:20:30 +0900

    Fix attribute error

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import ItemList, ItemAddView, ItemShowView, ItemEditView, ItemDeleteView
 
-appname='shoppinglist'
+app_name = 'shoppinglist'
 urlpatterns = [
-    path('list/', ItemList.as_view(),name='list'),
-    path('add', ItemAddView.as_view(),name='add'),
-    path('show', ItemShowView.as_view(),name='show'),
-    path('edit', ItemEditView.as_view(),name='edit'),
-    path('delete', ItemDeleteView.as_view(),name='delete'),
+    path('list/', ItemList.as_view(), name='list'),
+    path('add', ItemAddView.as_view(), name='add'),
+    path('show', ItemShowView.as_view(), name='show'),
+    path('edit', ItemEditView.as_view(), name='edit'),
+    path('delete', ItemDeleteView.as_view(), name='delete'),
 ]
